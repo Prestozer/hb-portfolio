adebac8c9f36d808ca868daf237fe17e28b63f22
MarloneA <marlone911@gmail.com>
2021-04-14 13:29:42 +0300

    add contact pages

---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./home.css";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div className="container mt-40">
+        <h3>Welcome to hb-portfolio</h3>
+        <p>These are some of my works</p>
+      </div>
+    );
+  }
+}
